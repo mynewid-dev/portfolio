bee60f08ecdf3093a5d5d30aeb95034a240ad114
rakshit-singh1 <thisisjames007@gmail.com>
2025-01-29 13:45:30 +0530

    new task

---
+++ assets/Home-Bgq2ubqe.js
@@ -0,0 +1,1 @@
+import{j as o,T as e}from"./index-BYmNo3-5.js";import{C as t}from"./Container-Cu6Glv9a.js";const s=()=>o.jsxs(t,{children:[o.jsx(e,{variant:"h4",gutterBottom:!0,children:"Welcome to My Portfolio"}),o.jsx(e,{children:"I am a Blockchain and React.js developer with expertise in building web3 solutions."})]});export{s as default};
---
+++ assets/Tools-C8imxBao.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BYmNo3-5.js";const r=()=>o.jsx("div",{children:"Tools"});export{r as default};
